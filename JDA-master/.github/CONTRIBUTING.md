e10cbfff9f258d792d38e1768922a666293af501
jin <jin.zhen@stonybrook.edu>
2018-10-03 01:26:44 -0400

    Bot passes by captcha at times. Need to find a permanent solution. Still need to parse data & send to user. Might use pastebin API

---
+++ JDA-master/.github/CONTRIBUTING.md
@@ -0,0 +1,33 @@
+
+## Making Changes
+
+Depending on your changes there are certain rules you have to follow if you expect
+your Pull Request to be merged.
+
+**Note**: It is recommended to create a new remote branch for each Pull Request. 
+Based on the current `upstream/development` changes!
+
+1. Adding a new Method or Class
+    - If your addition is not internal (e.g. an impl class or private method) you have to write documentation.
+        - For that please follow the [JavaDoc template](https://github.com/DV8FromTheWorld/JDA/wiki/6%29-JDA-Structure-Guide#javadoc)
+    - Keep your code consistent! [example](https://github.com/DV8FromTheWorld/JDA/wiki/5%29-contributing#examples)
+        - Follow the guides provided at [JDA Structure Guide](https://github.com/DV8FromTheWorld/JDA/wiki/6%29-JDA-Structure-Guide)
+        - Compare your code style to the one used all over JDA and ensure you
+          do not break the consistency (if you find issues in the JDA style you can include and update it)
+    - Do not remove existing functionality, use deprecation instead (for reference [deprecation policy](https://github.com/DV8FromTheWorld/JDA#deprecation-policy))
+
+2. Making a Commit
+    - While having multiple commits can help the reader understand your changes, it might sometimes be
+      better to include more changes in a single commit.
+    - When you commit your changes write a proper commit caption which explains what you have done
+
+3. Updating your Fork
+    - Before you start committing make sure your fork is updated.
+      (See [Syncing a Fork](https://help.github.com/articles/syncing-a-fork/)
+      or [Keeping a Fork Updated](https://robots.thoughtbot.com/keeping-a-github-fork-updated))
+      
+4. Only open Pull Requests to feature/experimental/development
+    - Look at the [Repository Structure](https://github.com/DV8FromTheWorld/JDA/wiki/11%29-Repository-Structure) for further details
+      
+For more information please consult the [Contributing](https://github.com/DV8FromTheWorld/JDA/wiki/5%29-Contributing)
+section of our wiki.
---
+++ src/main/java/DiscordBot.java
@@ -0,0 +1,124 @@
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.security.auth.login.LoginException;
+import java.util.Collections;
+
+import static java.lang.Thread.sleep;
+
+public class DiscordBot extends ListenerAdapter {
+
+    static ChromeOptions options;
+    static WebDriver driver;
+    static String email = "";
+    static String password = "";
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){
+        if(event.getAuthor().isBot()){
+            return;
+        }
+        System.out.println("Received message from " + event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
+        if(event.getMessage().getContentRaw().contains("!chegg")){
+            String[] messageContents = event.getMessage().getContentRaw().split(" ");
+            String cheggLink = messageContents[1];
+            String answer = null;
+            try {
+                answer = getAnswer(driver, cheggLink);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            event.getChannel().sendMessage(answer).queue();
+        }
+    };
+
+    public static void main(String[] args) throws InterruptedException, LoginException {
+        options = new ChromeOptions();
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+
+//        options.addArguments("headless");
+        options.addArguments("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36");
+
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/jinth/IdeaProjects/Flight-Price-Checker/chromedriver.exe");
+        driver = new ChromeDriver(options);
+        launch();
+    }
+    public static void loginGoogle(WebDriver driver, String email, String password) throws InterruptedException {
+        driver.navigate().to("https://gmail.com");
+        waitForLoad(driver);
+        sleep(2000);
+        WebElement emailBox = driver.findElement(By.id("identifierId"));
+        emailBox.sendKeys(email, Keys.ENTER);
+        sleep(2000);
+        WebElement passwordBox = driver.findElement(By.name("password"));
+        passwordBox.sendKeys(password, Keys.ENTER);
+        sleep(2000);
+    }
+    public static void waitForLoad(WebDriver driver) {
+        new WebDriverWait(driver, 30).until((ExpectedCondition<Boolean>) wd ->
+                ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
+    }
+    public static String getAnswer(WebDriver driver, String cheggLink) throws InterruptedException {
+        String pageSource, returnValue = "";
+        driver.navigate().to("https://textsheet.com/");
+        sleep(2000);
+        ((JavascriptExecutor)driver).executeScript("window.open();");
+        String parentHandle = driver.getWindowHandle(); // get the current window handle
+              //Clicking on this window
+        for (String winHandle : driver.getWindowHandles()) { //Gets the new window handle
+            driver.switchTo().window(winHandle);        // switch focus of WebDriver to the next found window handle (that's your newly opened window)
+        }
+        driver.get("https://www.youtube.com/");
+        sleep(500);
+        WebElement searchBox = driver.findElement(By.id("search"));
+        searchBox.sendKeys(cheggLink, Keys.chord(Keys.CONTROL, "a"), Keys.chord(Keys.CONTROL, "c"));
+        driver.switchTo().window(parentHandle);
+
+
+
+        WebElement inputBox = driver.findElement(By.id("1"));
+
+        inputBox.sendKeys(Keys.chord(Keys.CONTROL, "v"), Keys.ENTER);
+
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, 15);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("blur")));
+            pageSource = driver.getPageSource();
+            returnValue = parsePageSource(pageSource);
+//            System.out.println(returnValue);
+        }
+        catch (Exception e){
+            returnValue = "Loading took too long. Try again maybe?";
+        }
+        return returnValue;
+    }
+    public static void launch() throws LoginException, InterruptedException {
+        JDABuilder builder = new JDABuilder((AccountType.BOT));
+        String token = "";
+        builder.addEventListener(new DiscordBot());
+        builder.setToken(token).build();
+        loginGoogle(driver, email, password);
+
+    }
+    public static String parsePageSource(String pageSource){
+//        System.out.println(pageSource);
+        String removedHeader = pageSource.substring(pageSource.indexOf("<div id=\"content\">"));
+
+        //Removes majority of the tags.
+        String removedTags = removedHeader.replaceAll("<p>","").replaceAll("</p>", "").replaceAll(
+                "<img alt=\"\">", "").replaceAll(" src=", "").replaceAll("/>", "");
+
+        String result = removedTags.substring(0, removedTags.indexOf("</div>"));
+        System.out.println(result);
+        return result;
+    }
+}
